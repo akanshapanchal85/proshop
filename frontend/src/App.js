802dee71addc81021503abb70a64d4f9cffff553
Akansha <pakansha914@gmail.com>
2022-01-27 22:21:26 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to Proshop </h1>
-    </>
+    
+  
+      <Router>
+      <Header/>
+      <main className='py-3'>
+      <Container>
+      <Routes>
+          <Route path="/" element={<HomeScreen/>} />
+          <Route path="/product/:id" element={<ProductScreen/>} />
+      </Routes>
+      </Container>
+      </main>
+      <Footer/>
+      </Router>
+   
   );
 }
 
